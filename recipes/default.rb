20695b666795b08e55ec229d5cfad08c8340991e
luis rodriguez <luis.rodriguez@cru.org>
2014-12-09 16:36:02 -0500

    change to using only one condition only_if "grep tuxedo /etc/passwd", :environment => { 'HOME' => "/home/tuxedo" }

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,6 @@ template "#{ora_home}/network/admin/tnsnames.ora" do
               :db_server => node['tns']['db_server'],
               :service => node['tns']['app_service']
               )
-only_if { "grep tuxedo /etc/passwd", :environment => { 'HOME' => "/home/tuxedo" } && File.exist?("#{ora_home}/network/admin/tnsnames.ora") }
+only_if "grep tuxedo /etc/passwd", :environment => { 'HOME' => "/home/tuxedo" }
 end
 
